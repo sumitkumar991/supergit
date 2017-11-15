062132667569020a7329bd9eb65a22a3aeffa868
Sumit <kumar1993sumit15@gmail.com>
2017-11-15 12:48:03 +0530

    functionality to add all files recursively to head (like git add .)

--- src/gitcloj/core.clj
+++ src/gitcloj/core.clj
@@ -1,13 +1,11 @@
 (ns gitcloj.core
   (:gen-class)
   (require [clojure.java.io :as io])
-  (require [gitcloj.reader :as rd]))
-(defn read-all-file
-  [directory]
-  (.listFiles (io/file directory)))
+  (require [gitcloj.reader :as rd])
+  (require [clojure.string :as cstr]))
+
 (def root ".clogit/")
 (def headfile ".clogit/Head")
-
 (defn create-directories
   [parent]
   (do
@@ -29,15 +27,38 @@
   (if (.isDirectory (io/file (str parent root)))
     (println "The Directory is already under vcs")
     (create-directories parent)))
+
+(defn read-files-recursively
+  "Helper method of read-relative-paths to get absolute paths of all files from parent directory"
+  ([directory]
+   (let [
+         allfiles (flatten
+                    (let [filelist (remove #(cstr/starts-with? % ".clogit") (seq (.list (io/file directory))))]
+                      (for [dir filelist]
+                        (if (.isDirectory (io/file directory dir))
+                          (read-files-recursively (str directory dir "/"))
+                          (str directory dir)))))
+         ]
+     allfiles)))
+
+(defn read-relative-paths
+  "Call this method to get a list with relative paths of all the files from the given root directory"
+  [parent-dir]
+  (map #(cstr/replace-first % parent-dir "") (read-files-recursively parent-dir)))
+
 (defn add
-  [parent fpath]
-  (if (= "." fpath)
-    "do something here"
-    (let [head (rd/get-head parent) fls (get-in head [:current :files])]
-      (println head fpath)
-      (spit (str parent headfile)
+  [parent-dir fpath]
+  (let [head (rd/get-head parent-dir) fls (get-in head [:current :files])]
+    ;(println head fpath)
+    (if (= "." fpath)
+      (let [fileset  (apply hash-set (read-relative-paths parent-dir))]
+        (spit (str parent-dir headfile)
+              (update-in
+                head [:current :files] #(apply merge % fileset))))
+      (spit (str parent-dir headfile)
             (update-in
               head [:current :files] conj fpath)))))
+
 (defn status
   [parent]
   (let [head (rd/get-head parent) files (get-in head [:current :files])]
